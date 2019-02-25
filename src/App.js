20e9541bda34d45938e8174ea953f4e006c4ec79
AlexVestin <aleve030@student.liu.se>
2019-02-25 19:13:57 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,121 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { setShader, initBuffers, drawScene, createTexture } from "./gl.js";
+import "./App.css";
+
+const vsSource = `#version 300 es
+in vec4 aVertexPosition;
+void main() {
+  gl_Position =  aVertexPosition;
+}`;
+
+const fsSource = `#version 300 es
+  precision mediump float;
+  uniform vec2 iResolution;
+  uniform float iTime;
+  uniform vec2 iMouse;
+  out vec4 fragColor;
+
+  void mainImage(out vec4 fragColor, in vec2 fragCoord);
+  void main() {
+    mainImage(fragColor, gl_FragCoord.xy);
+  }
+  \n
+  `;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.ref = React.createRef();
+  }
+  componentDidMount() {
+    this.canvas = this.ref.current;
+    this.canvas.width = 1280;
+    this.canvas.height = 720;
+    this.canvas.style.backgroundColor = "black";
+    this.gl = this.canvas.getContext("webgl2");
+
+    const key = "ftrKwh";
+    const shaderId = "3s23Wt";
+    const url = `https://www.shadertoy.com/api/v1/shaders/${shaderId}?key=${key}`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(text => this.parseResponse(text.Shader));
+
+    this.passes = [];
+    //this.initGl("");
+  }
+
+  initPass = pass => {
+    const buffers = pass.inputs.filter(input => input.ctype === "buffer");
+    const channels = buffers.map(pass =>"iChannel" + String(pass.channel));
+
+    let fsShader = fsSource.split("\n");
+    channels.forEach(channel => {
+      fsShader.splice(3, 0, "uniform sampler2D " + channel + ";\n");
+    });
+
+    this.shaderProgram = setShader(this.gl, vsSource, fsShader.join("\n") + pass.code);;
+    const channelLocations = channels.map(channel =>this.gl.getUniformLocation(this.shaderProgram, channel));
+    
+
+    this.passes.push({
+      screenWidth: 1280,
+      screenHeight: 720,
+      program: this.shaderProgram,
+      attribLocations: {
+        vertexPosition: this.gl.getAttribLocation(
+          this.shaderProgram,
+          "aVertexPosition"
+        )
+      },
+      uniformLocations: {
+        iTime:  this.gl.getUniformLocation(
+          this.shaderProgram,
+          "iTime"
+        ),
+        resolution: this.gl.getUniformLocation(
+          this.shaderProgram,
+          "iResolution"
+        )
+      },
+      bufferLocations: {
+        ...channelLocations
+      }
+    });
+
+    this.buffers = initBuffers(this.gl);
+    requestAnimationFrame(this.animate);
+  };
+
+  animate = () => {
+    this.passes.forEach((pass, i) => {
+      if(i < this.passes.length)
+      drawScene(this.gl, pass, this.buffers, this.readBuffer, this.writeBuffer, i === this.passes.length - 1);
+    })
+    
+    requestAnimationFrame(this.animate);
+  };
+
+  parseResponse = response => {
+    console.log(response)
+    if(response) {
+      this.readBuffer = createTexture(this.gl, 1280, 720);
+      this.writeBuffer = createTexture(this.gl, 1280, 720);
+  
+      response.renderpass.forEach(pass => {
+        this.initPass(pass);
+      })
+    }else {
+      console.log("no api")
+    }
+   
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <canvas ref={this.ref} />
         </header>
       </div>
     );
---
+++ src/gl.js
@@ -0,0 +1,186 @@
+
+
+
+class Texture {
+    constructor(gl, width, height) {
+        const targetTextureWidth = width;
+        const targetTextureHeight = height;
+        const targetTexture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, targetTexture);
+        
+    
+        // define size and format of level 0
+        this.level = 0;
+        const internalFormat = gl.RGBA;
+        const border = 0;
+        const format = gl.RGBA;
+        const type = gl.UNSIGNED_BYTE;
+        const data = null;
+        gl.texImage2D(gl.TEXTURE_2D, this.level, internalFormat,
+                        targetTextureWidth, targetTextureHeight, border,
+                        format, type, data);
+        
+        // set the filtering so we don't need mips
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    
+        this.width  = width;
+        this.height = height;
+        this.texture = targetTexture;
+        this.fbo    = createFBO(gl, targetTexture, this.level);
+    }
+}
+
+export function setUp(gl) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+export function createTexture(gl, width, height) {
+    return new Texture(gl, width, height);
+}
+
+export function createFBO(gl, tex, level) {
+    // Create and bind the framebuffer
+    const fb = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+    
+    // attach the texture as the first color attachment
+    const attachmentPoint = gl.COLOR_ATTACHMENT0;
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, attachmentPoint, gl.TEXTURE_2D, tex, level);
+    return fb;
+}
+
+export function drawScene(gl, programInfo, buffers, readBuffer, writeBuffer, toScreen) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+    gl.clearDepth(1.0);                 // Clear everything
+    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+  
+    // Clear the canvas before we start drawing on it.
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    {
+      const numComponents = 2;  // pull out 2 values per iteration
+      const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+      const normalize = false;  // don't normalize
+      const stride = 0;         // how many bytes to get from one set of values to the next
+                                // 0 = use type and numComponents above
+      const offset = 0;         // how many bytes inside the buffer to start from
+      gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+      gl.vertexAttribPointer(
+          programInfo.attribLocations.vertexPosition,
+          numComponents,
+          type,
+          normalize,
+          stride,
+          offset);
+      gl.enableVertexAttribArray(
+          programInfo.attribLocations.vertexPosition);
+    }
+  
+    // Tell WebGL to use our program when drawing
+  
+    gl.useProgram(programInfo.program);
+
+    const resolution = [1280, 720];
+    gl.uniform2f(
+        programInfo.uniformLocations.resolution,
+        resolution[0],
+        resolution[1]
+    );
+
+    gl.uniform1f(
+        programInfo.uniformLocations.iTime,
+        performance.now() / 1000
+    )
+
+    if(!toScreen) {
+        gl.bindFramebuffer(gl.FRAMEBUFFER, readBuffer.fbo);
+        gl.bindTexture(gl.TEXTURE_2D, writeBuffer.texture);
+        gl.uniform1i(programInfo.bufferLocations[0], 0);
+        gl.viewport(0, 0, readBuffer.width, readBuffer.height);
+    }else {
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        gl.viewport(0, 0, programInfo.screenWidth, programInfo.screenHeight);
+    }
+    
+    {
+      const offset = 0;
+      const vertexCount = 4;
+      gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+    }
+}
+
+export function initBuffers(gl) {
+  const positionBuffer = gl.createBuffer();
+
+  // Select the positionBuffer as the one to apply buffer
+  // operations to from here out.
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // Now create an array of positions for the square.
+
+  const positions = [
+    -1.0,  1.0,
+     1.0,  1.0,
+    -1.0, -1.0,
+     1.0, -1.0,
+  ];
+
+  // Now pass the list of positions into WebGL to build the
+  // shape. We do this by creating a Float32Array from the
+  // JavaScript array, then use it to fill the current buffer.
+
+  gl.bufferData(gl.ARRAY_BUFFER,
+                new Float32Array(positions),
+                gl.STATIC_DRAW);
+
+  return {
+    position: positionBuffer,
+  };
+}
+
+export function setShader(gl, vsSource, fsSource) {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+  
+    // If creating the shader program failed, alert
+  
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+      alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+      return null;
+    }
+  
+    return shaderProgram;
+}
+
+export function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+  
+    // Send the source to the shader object
+  
+    gl.shaderSource(shader, source);
+  
+    // Compile the shader program
+  
+    gl.compileShader(shader);
+  
+    // See if it compiled successfully
+  
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+      gl.deleteShader(shader);
+      return null;
+    }
+  
+    return shader;
+  }
